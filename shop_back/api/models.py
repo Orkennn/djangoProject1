2831d73b2fb4ef121a0c6614d82aa65f88d948fe
Orkennn <sabit_orken@mail.ru>
2024-01-15 00:47:07 +0600

    Initial commit

---
+++ shop_back/api/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.FloatField()
+    description = models.TextField()
+    quantity = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
